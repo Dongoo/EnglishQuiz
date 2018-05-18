ff59f6213135599e5d991cac6c58cce7b67e69e3
Dongoo <34747264+Dongoo@users.noreply.github.com>
2018-05-18 11:31:23 +0200

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -137,8 +137,32 @@ function() {
 }();
 */
 
-function selezione(){
+
+function selezione_diff()
+{
   let e = document.getElementById("sel");
   let diff = e.options[e.selectedIndex].text;
   document.getElementById("display_diff").innerHTML = "Difficulty : " + diff;
+}
+
+function selezione_tempo()
+{
+  $("#time").on('blur change click dblclick error focus focusin focusout hover keydown keypress keyup load mousedown mouseenter mouseleave mousemove mouseout mouseover mouseup resize scroll select submit', 
+            function() { 
+              $("#display_time").text("Time : " + $("#time").val())
+            });
+}
+
+function selezione_numQ()
+{
+  $("#quest").on("change click dblclick error focus focusin focusout hover keydown keypress keyup load mousedown mouseenter mouseleave mousemove mouseout mouseover mouseup resize scroll select submit", 
+    function() { 
+      $("#display_numQ").text("Number questions : " + $("#quest").val())
+    });
+}
+
+// !! ATTENZIONE !! RICORDATI DI CREARE LA CLASSE PER LA GIOCATA
+function avvio_gioco()
+{
+  //BOI, WHERE THE GAME PAGE AT?
 }
